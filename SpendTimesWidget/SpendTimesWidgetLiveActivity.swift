8e3c04a4c2607be39e9145769b032b5659002d20
Jintaro Nishihata <jintaro@jnishihata.com>
2026-05-28 00:44:59 +0900

    Dynamic Islandのデザインをちょこっと改善

--- SpendTimesWidget/SpendTimesWidgetLiveActivity.swift
+++ SpendTimesWidget/SpendTimesWidgetLiveActivity.swift
@@ -14,50 +14,54 @@ struct SpendTimesWidgetLiveActivity: Widget {
         ActivityConfiguration(for: SpendTimesAttributes.self) { context in
             // ロック画面・バナー表示
             HStack {
-                Image(systemName: "timer")
+                
+                
                 Text(context.state.startDate, style: .timer)
                     .monospacedDigit()
                 Text(context.attributes.appName)
                     .foregroundStyle(.secondary)
+                
             }
             .padding()
         } dynamicIsland: { context in
             DynamicIsland {
                 // 展開時
-                DynamicIslandExpandedRegion(.center) {
+                DynamicIslandExpandedRegion(.leading){
+                    Image(systemName: "timer")
+                        .foregroundStyle(.cyan)
+                        .font(.system(size:44))
+                }
+                DynamicIslandExpandedRegion(.center){
                     HStack {
-                        Image(systemName: "timer")
-                        Text(context.state.startDate, style: .timer)
-                            .monospacedDigit()
-                            .font(.title2)
+                        Text(context.attributes.appName)
+                            .foregroundStyle(.secondary)
+                            .font(.callout)
+                            .multilineTextAlignment(.trailing)
                     }
                 }
-                DynamicIslandExpandedRegion(.bottom) {
-                    Text(context.attributes.appName)
-                        .foregroundStyle(.secondary)
-                        .font(.caption)
+                DynamicIslandExpandedRegion(.trailing) {
+                    Text(context.state.startDate, style: .timer)
+                        .monospacedDigit()
+                        .font(.system(size:44))
+                        .fontWeight(.semibold)
                 }
             } compactLeading: {
-                // コンパクト左（カメラ左側）
                 Image(systemName: "timer")
-                    .foregroundStyle(.green)
+                    .foregroundStyle(.cyan)
             } compactTrailing: {
-                // コンパクト右（カメラ右側）
                 Text(context.state.startDate, style: .timer)
                     .monospacedDigit()
                     .font(.caption2)
                     .frame(width: 40)
             } minimal: {
-                // 最小表示
                 Image(systemName: "timer")
             }
         }
     }
 }
 
-//#Preview("Notification", as: .content, using: SpendTimesWidgetAttributes.preview) {
-//   SpendTimesWidgetLiveActivity()
-//} contentStates: {
-//    SpendTimesWidgetAttributes.ContentState.smiley
-//    SpendTimesWidgetAttributes.ContentState.starEyes
-//}
+#Preview("Dynamic Island", as: .dynamicIsland(.expanded), using: SpendTimesAttributes(appName: "X")) {
+    SpendTimesWidgetLiveActivity()
+} contentStates: {
+    SpendTimesAttributes.ContentState(startDate: .now)
+}
