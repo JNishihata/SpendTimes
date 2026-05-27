0db9f689cfbe099c0c44dcf70d04ad6725b885c5
Jintaro Nishihata <jintaro@jnishihata.com>
2026-05-27 21:24:28 +0900

    Initial Commit

---
+++ Spend times/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spend times
+//
+//  Created by Jintaro Nishihata on 2026/05/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spend times/Spend_timesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spend_timesApp.swift
+//  Spend times
+//
+//  Created by Jintaro Nishihata on 2026/05/27.
+//
+
+import SwiftUI
+
+@main
+struct Spend_timesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
